04a575912f6f8d83248cb8ee4db7323123930d2a
ashwanibakshi <ab324002@gmail.com>
2023-09-05 13:14:13 +0530

    commit

---
+++ conditionalStatement.py
@@ -0,0 +1,14 @@
+#conditional statements in python
+
+#if
+a = int(input("enter value of a"))
+b = int(input("enter value of b"))
+
+if a<b:
+    print("a is less than b")
+
+if a>b:
+    print("a is greater than b")
+
+if a==b:
+    print("a is equal to b")
