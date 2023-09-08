7d02cc1548ea4df7e758ebb4ab763923e52043aa
ashwanibakshi <ab324002@gmail.com>
2023-09-08 16:06:38 +0530

    commit

--- loops.py
+++ loops.py
@@ -32,4 +32,37 @@ print("\n")
 
 #print data using index in for loop
 for s in range(len(students)): 
-    print(students[s],s)   
+    print(students[s],s)  
+
+#dictionary(dict)
+
+students = {
+    "adam":"red house",
+    "john":"red house",
+    "paul":"green house"
+} 
+
+print(students["adam"])
+print(students["john"])
+print(students["paul"])
+
+for student in students:
+    print(student,students[student],sep=", ")
+
+#here we are using dictionary("{}") in list("[]")
+studentss = [
+{"name":"adam","house":"red house","class":"6"},
+{"name":"john","house":"red house","class":"7"},
+{"name":"paul","house":"green house","class":"8"}
+]
+
+for student in studentss:
+    print(student["name"],student["house"],student["class"],sep=",")
+
+#using outer/inner for loop
+size = int(input("enter width: "))
+
+for i in range(size):
+    for j in range(size):
+        print("#",end="")
+    print()
